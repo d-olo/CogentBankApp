3471f18be0106d4bc9e2902306da2d710b2a64f8
Dionel Olo <dionelblaise@gmail.com>
2022-03-16 14:31:44 -0400

    Fix for CORS error

--- BankingApplication/src/main/java/com/learning/controller/CustomerController.java
+++ BankingApplication/src/main/java/com/learning/controller/CustomerController.java
@@ -22,6 +22,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -72,6 +73,7 @@ import com.learning.utils.FileUploadUtil;
 
 @RestController
 @RequestMapping("/customer")
+@CrossOrigin(origins = "http://localhost:4200")
 /**
  * Handler for the customer API.
  * @author Dionel Olo, Oliver Pagalanan
