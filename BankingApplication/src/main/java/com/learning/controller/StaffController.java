509fe75573611aeda726940fc88766dcf5412f24
Dionel Olo <dionelblaise@gmail.com>
2022-03-16 20:02:35 -0400

    Added @CrossOrigin annotation to both admin and staff controllers.

--- BankingApplication/src/main/java/com/learning/controller/StaffController.java
+++ BankingApplication/src/main/java/com/learning/controller/StaffController.java
@@ -13,6 +13,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +48,7 @@ import com.learning.service.AccountService;
 
 @RestController
 @RequestMapping("/staff")
+@CrossOrigin(origins = "http://localhost:4200")
 /**
  * Handler for the staff API.
  * @author Dionel Olo
